a3269f768d08066c7d333dccc74385888cbe0c2f
Mohamed Saber <mohamedsaberahmed@std.mans.edu.eg>
2026-09-21 08:04:28 +0300

    fix(db): add fallback connection string in Persistence and inject connection string into web.config

--- Stock-Exchange.Persistance/DependencyInjection.cs
+++ Stock-Exchange.Persistance/DependencyInjection.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Stock_Exchange.Application.Common.Interfaces;
@@ -9,9 +9,15 @@ namespace Stock_Exchange.Persistance
     {
         public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
         {
+            var defaultConnectionString = "Server=db69245.public.databaseasp.net; Database=db69245; User Id=db69245; Password=Mo@123456; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True;";
+            var connectionString = configuration.GetConnectionString("StockExchangeConnectionString")
+                ?? configuration.GetConnectionString("CareClinicHubDb")
+                ?? configuration["ConnectionStrings:StockExchangeConnectionString"]
+                ?? defaultConnectionString;
+
             services.AddDbContext<StockExchangeDbContext>(options =>
             {
-                options.UseSqlServer(configuration.GetConnectionString("StockExchangeConnectionString") ?? configuration.GetConnectionString("CareClinicHubDb"));
+                options.UseSqlServer(connectionString);
             });
 
             services.AddScoped<IStockExchangeDbContext>(provider => provider.GetRequiredService<StockExchangeDbContext>());
